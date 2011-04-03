josesimoes/TortoiseSVN

commit 90da06b74a179e2dbce33a1593a4bb0046263058

* add Invalidate() method to the dictionary based path classes * in the log iterator classes, Invalidate() the path member   after we hit the initial ADD
git-svn-id: https://svn.osdn.net/svnroot/tortoisesvn/trunk@21026 154be670-7969-47af-9ab0-c6fb378d2295

src/LogCache/Access/LogIteratorBase.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2009 - TortoiseSVN
+// Copyright (C) 2007-2009,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -205,7 +205,7 @@ bool CLogIteratorBase::InternalHandleCopyAndDelete
                     // /trunk/project
                     // is added first (usually without a copy from path)
                     //
-                    // Stop iteration only if we found and ADD of
+                    // Stop iteration only if we found an ADD of
                     // the exact search path.
 
                     if (searchPath == changedPath)
@@ -213,6 +213,7 @@ bool CLogIteratorBase::InternalHandleCopyAndDelete
                         // the path we are following actually started here.
 
                         searchRevision = (revision_t)NO_REVISION;
+                        searchPath.Invalidate();
                         return true;
                     }
                 }

src/LogCache/Containers/DictionaryBasedTempPath.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2007 - TortoiseSVN
+// Copyright (C) 2007,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -118,6 +118,12 @@ public:
     bool IsValid() const
     {
         return inherited::IsValid();
+    }
+
+    void Invalidate()
+    {
+        inherited::Invalidate();
+        relPathElements.clear();
     }
 
     bool IsFullyCachedPath() const

src/LogCache/Containers/PathDictionary.h
@@ -264,6 +264,11 @@ public:
         return index != NO_INDEX;
     }
 
+    void Invalidate()
+    {
+        index = (index_t)NO_INDEX;
+    }
+
     CDictionaryBasedPath GetParent() const
     {
         return CDictionaryBasedPath ( dictionary

commit 2a346979528768442b258574159b2b414f0766c6

fix an issue introduced in r21026: * make copy targets (esp. the initial add) available in iterators * use that to find the initial path name in CFullHistory
git-svn-id: https://svn.osdn.net/svnroot/tortoisesvn/trunk@21103 154be670-7969-47af-9ab0-c6fb378d2295

src/LogCache/Access/ILogIterator.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2007 - TortoiseSVN
+// Copyright (C) 2007,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -49,7 +49,9 @@ public:
 
     virtual bool DataIsMissing() const = 0;
     virtual revision_t GetRevision() const = 0;
+    virtual revision_t GetAddRevision() const = 0;
     virtual const CDictionaryBasedTempPath& GetPath() const = 0;
+    virtual const CDictionaryBasedTempPath& GetAddPath() const = 0;
     virtual bool EndOfPath() const = 0;
 
     /// to next / previous revision for our path

src/LogCache/Access/LogIteratorBase.cpp
@@ -134,16 +134,14 @@ bool CLogIteratorBase::ContainsCopyOrDelete
 bool CLogIteratorBase::InternalHandleCopyAndDelete
     ( const CRevisionInfoContainer::CChangesIterator& first
     , const CRevisionInfoContainer::CChangesIterator& last
-    , const CDictionaryBasedPath& revisionRootPath
-    , CDictionaryBasedTempPath& searchPath
-    , revision_t& searchRevision)
+    , const CDictionaryBasedPath& revisionRootPath)
 {
     // any chance that this revision affects our search path?
 
     if (!revisionRootPath.IsValid())
         return false;
 
-    if (!revisionRootPath.IsSameOrParentOf (searchPath.GetBasePath()))
+    if (!revisionRootPath.IsSameOrParentOf (path.GetBasePath()))
         return false;
 
     // close examination of all changes
@@ -164,7 +162,7 @@ bool CLogIteratorBase::InternalHandleCopyAndDelete
         // -> skip, if our search path is not affected (only some sub-path)
 
         CDictionaryBasedPath changedPath = iter->GetPath();
-        if (!changedPath.IsSameOrParentOf (searchPath.GetBasePath()))
+        if (!changedPath.IsSameOrParentOf (path.GetBasePath()))
             continue;
 
         // now, this is serious
@@ -208,12 +206,15 @@ bool CLogIteratorBase::InternalHandleCopyAndDelete
                     // Stop iteration only if we found an ADD of
                     // the exact search path.
 
-                    if (searchPath == changedPath)
+                    if (path == changedPath)
                     {
                         // the path we are following actually started here.
 
-                        searchRevision = (revision_t)NO_REVISION;
-                        searchPath.Invalidate();
+                        addRevision = revision;
+                        addPath = path;
+
+                        revision = (revision_t)NO_REVISION;
+                        path.Invalidate();
                         return true;
                     }
                 }
@@ -240,9 +241,12 @@ bool CLogIteratorBase::InternalHandleCopyAndDelete
 
     if (bestRename != last)
     {
-        searchPath = searchPath.ReplaceParent ( bestRename.GetPath()
-                                              , bestRename.GetFromPath());
-        searchRevision = bestRename.GetFromRevision();
+        addRevision = revision;
+        addPath = path;
+
+        path = path.ReplaceParent ( bestRename.GetPath()
+                                  , bestRename.GetFromPath());
+        revision = bestRename.GetFromRevision();
 
         return true;
     }
@@ -308,7 +312,9 @@ CLogIteratorBase::CLogIteratorBase ( const CCachedLogInfo* cachedLog
     , revisionIndices (cachedLog->GetRevisions())
     , skipRevisionInfo (cachedLog->GetSkippedRevisions())
     , revision (startRevision)
+    , addRevision ((revision_t)NO_REVISION)
     , path (startPath)
+    , addPath (startPath.GetDictionary())
 {
 }
 

src/LogCache/Access/LogIteratorBase.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2008 - TortoiseSVN
+// Copyright (C) 2007-2008,2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -54,6 +54,11 @@ protected:
     revision_t revision;
     CDictionaryBasedTempPath path;
 
+    // last position before we hit the last copy operation
+
+    revision_t addRevision;
+    CDictionaryBasedTempPath addPath;
+
     // construction
     // (copy construction & assignment use default methods)
 
@@ -92,12 +97,10 @@ protected:
 
     // return true, searchPath / searchRevision have been modified
 
-    static bool InternalHandleCopyAndDelete
+    bool InternalHandleCopyAndDelete
         ( const CRevisionInfoContainer::CChangesIterator& first
         , const CRevisionInfoContainer::CChangesIterator& last
-        , const CDictionaryBasedPath& revisionRootPath
-        , CDictionaryBasedTempPath& searchPath
-        , revision_t& searchRevision);
+        , const CDictionaryBasedPath& revisionRootPath);
 
     // log scanning sub-routines
 
@@ -123,7 +126,9 @@ public:
 
     virtual bool DataIsMissing() const;
     virtual revision_t GetRevision() const;
+    virtual revision_t GetAddRevision() const;
     virtual const CDictionaryBasedTempPath& GetPath() const;
+    virtual const CDictionaryBasedTempPath& GetAddPath() const;
     virtual bool EndOfPath() const;
 
     virtual void Advance (revision_t last = 0);
@@ -156,9 +161,19 @@ inline revision_t CLogIteratorBase::GetRevision() const
     return revision;
 }
 
+inline revision_t CLogIteratorBase::GetAddRevision() const
+{
+    return addRevision;
+}
+
 inline const CDictionaryBasedTempPath& CLogIteratorBase::GetPath() const
 {
     return path;
+}
+
+inline const CDictionaryBasedTempPath& CLogIteratorBase::GetAddPath() const
+{
+    return addPath;
 }
 
 inline bool CLogIteratorBase::EndOfPath() const

src/LogCache/Access/StrictLogIterator.cpp
@@ -38,9 +38,7 @@ bool CStrictLogIterator::HandleCopyAndDelete()
 
     bool result = InternalHandleCopyAndDelete ( revisionInfo.GetChangesBegin (index)
                                               , revisionInfo.GetChangesEnd (index)
-                                              , revisionInfo.GetRootPath (index)
-                                              , path
-                                              , revision);
+                                              , revisionInfo.GetRootPath (index));
     if (result)
     {
         // stop on copy

src/TortoiseProc/RevisionGraph/FullHistory.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2010 - TortoiseSVN
+// Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -501,7 +501,7 @@ void CFullHistory::AnalyzeRevisionData()
         }
     }
 
-    *startPath = iterator.GetPath();
+    *startPath = iterator.GetAddPath();
 }
 
 inline bool AscendingFromRevision (const SCopyInfo* lhs, const SCopyInfo* rhs)